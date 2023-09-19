fa75bd37189d60a4e4432ff20a294ed9bdcb21f9
GLI81 <liguangzheng998@hotmail.com>
2023-09-19 13:58:53 -0400

    Edited test_lib

---
+++ test_lib.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+
+"""Test for lib.py"""
+
+from lib import get_desc_stats, get_hist
+import pandas as pd
+
+def test_get_desc_stats():
+    df = pd.read_csv("./resources/train.csv")
+    di = get_desc_stats(df, "Survived")
+    assert round(di["mean"], 4) == 0.3838
+    assert round(di["median"], 4) == 0.0
+    assert round(di["std"], 4) == 0.4866
+
+def test_get_hist():
+    get_hist()
